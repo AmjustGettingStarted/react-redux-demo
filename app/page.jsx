07ba84c2af31a4b5b2a6d1b162dfb7157bbc6140
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-06-11 18:16:03 +0530

    final edit

--- app/page.jsx
+++ app/page.jsx
@@ -26,7 +26,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [page, setPage] = useState(1);
-  const [newPost, setNewPost] = useState({ body: "", title: "" });
+  const [newPost, setNewPost] = useState({ name: "", email: "" });
   const { data, error, isLoading } = useGetPostsQuery(page);
 
   const [createPost, { isLoading: isCreating, error: createError }] =
@@ -52,12 +52,22 @@ export default function Home() {
       <div className="space-y-4 pb-4 ">
         <Input
           type="text"
-          name="title"
-          id="title"
-          placeholder="title"
+          name="name"
+          id="name"
+          placeholder="name"
           className="bg-white"
           onChange={(e) =>
-            setNewPost((prev) => ({ ...prev, title: e.target.value }))
+            setNewPost((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="email"
+          className="bg-white"
+          onChange={(e) =>
+            setNewPost((prev) => ({ ...prev, email: e.target.value }))
           }
         />
         <Input
@@ -86,11 +96,11 @@ export default function Home() {
             className="hover:scale-105 shadow-sm hover:shadow-md transition-all duration-300 hover:border-slate-500 hover:bg-slate-200 bg-slate-100 "
           >
             <CardHeader>
-              <CardTitle className="truncate flex flex-col space-y-2">
-                <p className="font-bold">
+              <CardTitle className="truncate flex flex-col space-y-1">
+                <p className="font-bold text-2xl">
                   {post.id}.{post.name}
                 </p>
-                <p className="font-medium">{post.email}</p>
+                <p className="font-medium text-lg">{post.email}</p>
               </CardTitle>
             </CardHeader>
             <CardContent className="line-clamp-3">
