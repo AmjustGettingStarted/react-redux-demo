0c2a1b2db5c0f9ce3861215a52cdc9dcd4ea4903
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-06-11 14:45:05 +0530

    data fethcing successful

--- app/page.jsx
+++ app/page.jsx
@@ -10,6 +10,9 @@ export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <div>{data.map((post,i)=>(
+        <p key={i}>{post.title}</p>
+      ))}</div>
     </div>
   );
 }
